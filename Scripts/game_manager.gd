67bb870d911a0452b18955c39d83120ea428bf94
opallo <opallo.collins@gmail.com>
2023-11-18 02:50:03 -0800

    added debug function

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -4,29 +4,33 @@ var planet_generator: PlanetGenerator = PlanetGenerator.new()
 @onready var debug_input = $CanvasLayer/LineEdit
 @onready var debug_label = $CanvasLayer/Label
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	#var planet_generator = PlanetGenerator.new()
-	
+
 	var home_planet = planet_generator.generate_home_planet()
 	show_planet_summary(home_planet)
-	
+
 	#var random_planet = planet_generator.generate_random_planet()
 	#show_planet_summary(random_planet)
-	
+
 	# Process console input
+
+
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.is_action_pressed("ui_accept"):
 			var input_text = debug_input.text  # Assuming you have a TextInput node
 			if input_text == "generate":
-				debug_label.text = "generating planet"
+				debug_log("generating planet")
 				var new_planet = planet_generator.generate_random_planet()
-				debug_label.text = "planet generated, showing planet summary"
+				debug_log("showing planet summary")
 				show_planet_summary(new_planet)
 				debug_input.select_all()
 				debug_input.clear()  # Clear input
+
 
 func show_planet_summary(planet: Planet):
 	print(planet.planet_name)
@@ -35,3 +39,6 @@ func show_planet_summary(planet: Planet):
 	print(planet.planet_conditions)
 	print(planet.planet_resources)
 
+
+func debug_log(message: String):
+	debug_label.text += message + "\n"
