df86f1af88e7d50149ac3b4729df42bfcf6b0ca5
Opallo <opallo.collins@gmail.com>
2023-11-18 01:33:52 -0800

    variable names for debug ui

--- game_manager.gd
+++ game_manager.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var planet_generator: PlanetGenerator = PlanetGenerator.new()
+@onready var debug_input = $CanvasLayer/LineEdit
+@onready var debug_label = $CanvasLayer/Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,12 +19,14 @@ func _ready():
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.is_action_pressed("ui_accept"):
-			var input_text = $LineEdit.text  # Assuming you have a TextInput node
+			var input_text = debug_input.text  # Assuming you have a TextInput node
 			if input_text == "generate":
+				debug_label.text = "generating planet"
 				var new_planet = planet_generator.generate_random_planet()
+				debug_label.text = "planet generated, showing planet summary"
 				show_planet_summary(new_planet)
-				$LineEdit.select_all()
-				$LineEdit.clear()  # Clear input
+				debug_input.select_all()
+				debug_input.clear()  # Clear input
 
 func show_planet_summary(planet: Planet):
 	print(planet.planet_name)
