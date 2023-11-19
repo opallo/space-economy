69b31ba932815455b6adcdef37a2fa4bdd55396a
Opallo <opallo.collins@gmail.com>
2023-11-19 02:21:09 -0800

    successfully implemented the use of a dictionary as the planet generation settings parameter

--- Scripts/basic_planet.gd
+++ Scripts/basic_planet.gd
@@ -9,19 +9,12 @@ var planet_resources: Dictionary
 var planet_population: int
 var planet_happiness: String
 
-func _init(_name: String, _type: String, _size: String, _conditions: String, _resources: Dictionary, _population: int, _happiness: String):
-	planet_name = _name
-	planet_type = _type
-	planet_size = _size
-	planet_conditions = _conditions
-	planet_resources = _resources
-	planet_population = _population
-	planet_happiness = _happiness
-
-#func _init(settings: Dictionary):
-#	planet_name = settings.get("planet_name", "DefaultName")
-#	planet_type = settings.get("planet_type", "DefaultType")
-#	planet_size = settings.get("planet_size", "DefaultSize")
-#	planet_conditions = settings.get("planet_conditions", "DefaultConditions")
-#	planet_resources = settings.get("planet_resources", {})
+func _init(settings: Dictionary):
+	planet_name = settings.get("planet_name", "DefaultName")
+	planet_type = settings.get("planet_type", "DefaultType")
+	planet_size = settings.get("planet_size", "DefaultSize")
+	planet_conditions = settings.get("planet_conditions", "DefaultConditions")
+	planet_resources = settings.get("planet_resources", {})
+	planet_population = settings.get("planet_population", 0)
+	planet_happiness = settings.get("planet_happiness", null)
 
--- Scripts/planet_generator.gd
+++ Scripts/planet_generator.gd
@@ -5,34 +5,37 @@ class_name PlanetGenerator
 # Possible planet attributes
 var planet_types = ["Rocky", "Ice", "Gas Giant", "Habitable"]
 var planet_sizes = ["Small", "Medium", "Large"]
-var planet_resources = ["Alloys", "Water", "Coal", "Hydrogen", "Uranium", "Silicon"]
+var planet_resources = ["Alloys", "Minerals", "Water", "Gas"]
 var planet_conditions = ["Temperate", "Arid", "Frozen", "Toxic"]
 var planet_population
 var planet_happiness_levels = ["Low", "Medium", "High"]
 
 func generate_home_planet() -> Planet:
 	var settings: HomePlanet = HomePlanet.new()
-	var home_planet: Planet = Planet.new(
-		settings.HOME_NAME,
-		settings.HOME_TYPE,
-		settings.HOME_SIZE,
-		settings.HOME_CONDITIONS,
-		settings.HOME_RESOURCES,
-		settings.HOME_POPULATION,
-		settings.HOME_HAPPINESS
-	)
+	var home_planet_attributes = {
+		"planet_name": settings.HOME_NAME,
+		"planet_type": settings.HOME_TYPE,
+		"planet_size": settings.HOME_SIZE,
+		"planet_conditions": settings.HOME_CONDITIONS,
+		"planet_resources": settings.HOME_RESOURCES,
+		"planet_population": settings.HOME_POPULATION,
+		"planet_happiness": settings.HOME_HAPPINESS
+	}
+	var home_planet: Planet = Planet.new(home_planet_attributes)
 	return home_planet
 
 func generate_random_planet() -> Planet:
-	var planet_name = "Planet_" + str(randi())  # Simple random name
-	var type = planet_types[randi() % planet_types.size()]
-	var size = planet_sizes[randi() % planet_sizes.size()]
-	var conditions = planet_conditions[randi() % planet_conditions.size()]
-	var resources = generate_resources()
-	var population = randi() % 100
-	var happiness = planet_happiness_levels[randi() % planet_happiness_levels.size()]
-	var new_planet = Planet.new(planet_name, type, size, conditions, resources, population, happiness)
-
+	var random_planet_attributes = {
+		"planet_name": "Planet_" + str(randi()),  # Simple random name	
+		"planet_type": planet_types[randi() % planet_types.size()],
+		"planet_size": planet_sizes[randi() % planet_sizes.size()],
+		"planet_conditions": planet_conditions[randi() % planet_conditions.size()],
+		"planet_resources": generate_resources(),
+		"planet_population": randi() % 100,
+		"planet_happiness": planet_happiness_levels[randi() % planet_happiness_levels.size()]
+	}
+	
+	var new_planet = Planet.new(random_planet_attributes)
 	return new_planet
 
 func generate_resources() -> Dictionary:
