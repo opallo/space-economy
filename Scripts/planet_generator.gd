0e350dd1ac20d419dcf1c6d515157f7426c5ae9f
opallo <opallo.collins@gmail.com>
2023-11-19 05:51:19 -0800

    + added planet_type_to_resources dictionary to use during planet generation

--- Scripts/planet_generator.gd
+++ Scripts/planet_generator.gd
@@ -3,37 +3,38 @@ extends Node
 class_name PlanetGenerator
 
 # Possible planet attributes
-var planet_types = ["Habitable", "Gas Giant", "Frozen", "Ocean", "Rocky"]
+var planet_types = ["Habitable", "Gas", "Frozen", "Ocean", "Rocky"]
 var planet_sizes = ["Small", "Medium", "Large"]
 var planet_resources = ["Alloys", "Minerals", "Water", "Gas"]
 var planet_conditions = ["Temperate", "Arid", "Frozen", "Toxic"]
 var planet_population
 var planet_happiness_levels = ["Low", "Medium", "High"]
 
+var planet_type_to_resources = {
+	"Habitable": {"Water": 100, "Minerals": 100, "Gas": 100, "Alloys": 100},
+	"Gas": {"Water": 0, "Minerals": 50, "Gas": 500, "Alloys": 10},
+	"Frozen": {"Water": 400, "Minerals": 50, "Gas": 0, "Alloys": 50},
+	"Ocean": {"Water": 500, "Minerals": 50, "Gas": 0, "Alloys": 10},
+	"Rocky": {"Water": 10, "Minerals": 200, "Gas": 0, "Alloys": 50}
+}
+
+
 func generate_random_planet() -> Planet:
+	var planet_type = planet_types[randi() % planet_types.size()]
+
 	var random_planet_attributes = {
-		"planet_name": "Planet_" + str(randi()),  # Simple random name	
-		"planet_type": planet_types[randi() % planet_types.size()],
+		"planet_name": "Planet_" + str(randi()),  # Simple random name
+		"planet_type": planet_type,
 		"planet_size": planet_sizes[randi() % planet_sizes.size()],
 		"planet_conditions": planet_conditions[randi() % planet_conditions.size()],
-		"planet_resources": generate_resources(),
+		"planet_resources": planet_type_to_resources[planet_type],
 		"planet_population": randi() % 100,
 		"planet_happiness": planet_happiness_levels[randi() % planet_happiness_levels.size()]
 	}
-	
+
 	var new_planet = Planet.new(random_planet_attributes)
 	return new_planet
 
-func generate_resources() -> Dictionary:
-	var resources = {}
-	var num_resources = randi() % planet_resources.size() + 1
-
-	for i in range(num_resources):
-		var resource = planet_resources[randi() % planet_resources.size()]
-		var amount = randi() % 1000
-		resources[resource] = amount
-
-	return resources
 
 func generate_home_planet() -> Planet:
 	var settings: HomePlanet = HomePlanet.new()
