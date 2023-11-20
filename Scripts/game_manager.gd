774267e134b9badc1c92844958ac1db5580a6fb0
opallo <opallo.collins@gmail.com>
2023-11-20 05:13:14 -0800

    Added button for generating planets

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,15 +3,18 @@ extends Node2D
 var planet_generator: PlanetGenerator = PlanetGenerator.new()
 @onready var debug_input = $CanvasLayer/LineEdit
 @onready var debug_label = $CanvasLayer/Label
+@onready var generate_planet_button = $CanvasLayer/GeneratePlanetButton
+
 
 func _ready():
 	randomize()
-	
+
 	debug_log("Generating home planet")
 	var home_planet = planet_generator.generate_home_planet()
 	debug_log("Showing home planet summary")
 	show_planet_summary(home_planet)
-	
+
+
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.is_action_pressed("ui_accept"):
@@ -24,6 +27,7 @@ func _input(event):
 				debug_input.select_all()
 				debug_input.clear()
 
+
 func show_planet_summary(planet: Planet):
 	print("Name: " + planet.planet_name)
 	print("Type: " + planet.planet_type)
@@ -35,6 +39,15 @@ func show_planet_summary(planet: Planet):
 	print(planet.planet_population)
 	print("Happiness: ")
 	print(planet.planet_happiness)
+	print("\n")
+
+
+func _on_generate_planet_button_pressed():
+	debug_log("Generating random planet")
+	var new_planet = planet_generator.generate_random_planet()
+	debug_log("Showing random planet summary")
+	show_planet_summary(new_planet)
+
 
 func debug_log(message: String):
 	debug_label.text += message + "\n"
